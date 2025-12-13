ad5f190eed8c06ca617639bce156f179174ecf84
NigeParis <nrobinso@student.42.fr>
2025-12-13 12:08:12 +0100

    basic setup pong - frontend and backend with simplw socket message and fastify route

--- frontend/src/pong/index.ts
+++ frontend/src/pong/index.ts
@@ -1,2 +1,1 @@
 import './pong.css';
-  
--- src/pong/src/sendGameLinkToChatService.ts
+++
@@ -1,7 +0,0 @@
-/**
-/* EXPERIMENTAL: how to send a starting game link to chat
-**/
-export async function sendGameLinkToChatService(link: string) :Promise<string> {
-	const payload = { link };
-	return JSON.stringify(payload);
-}
