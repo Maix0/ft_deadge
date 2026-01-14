16982488c90dc6f8a02560639b28a9d491994d14
bgoulard <bgoulard@student.42.fr>
2026-01-14 17:17:38 +0100

    eslint

--- src/pong/src/state.ts
+++ src/pong/src/state.ts
@@ -441,8 +441,7 @@ class StateI {
 				g_id + ' userId:' + sock.authUser.id);
 			return JoinRes.no;
 		}
-		if (game.userOnPage[0] === true && game.userOnPage[1] === true)
-		{
+		if (game.userOnPage[0] === true && game.userOnPage[1] === true) {
 			this.fastify.log.warn(
 				'user trying to connect to a game he\'s already joined: gameId:' +
 				g_id + ' userId:' + sock.authUser.id);
